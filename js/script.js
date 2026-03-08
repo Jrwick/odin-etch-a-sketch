2ed15900f646bf6363f62ecd410128004028f8ac
jrw <197988939+Jrwick@users.noreply.github.com>
2026-03-08 21:19:38 +0000

    Create the initial grid layout
    Created the boxes for the etch a sketch. Made the body a flexbox to
    center the whole grid.
    Locked the grid size at 50% vw no matter how many boxes are made.
    Uses aspect ratio to keep the inner divs square in shape.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("testing");
+const container = document.querySelector(".container");
+const numOfRows = 16;
+for (let i = 0; i < numOfRows; i++) {
+  const col = document.createElement("div");
+  col.classList.add("column");
+  for (let j = 0; j < numOfRows; j++) {
+    const row = document.createElement("div");
+    row.classList.add("node");
+    col.appendChild(row);
+  }
+  container.appendChild(col);
+}
