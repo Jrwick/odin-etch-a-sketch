ed962c3e89f73a1d3304cd6a8113fc2a744b3f7b
jrw <197988939+Jrwick@users.noreply.github.com>
2026-03-09 13:36:28 +0000

    Add option to change size of grid

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 const container = document.querySelector(".container");
-const numOfRows = 16;
-for (let i = 0; i < numOfRows; i++) {
-  const col = document.createElement("div");
-  col.classList.add("column");
-  for (let j = 0; j < numOfRows; j++) {
-    const row = document.createElement("div");
-    row.classList.add("node");
-    col.appendChild(row);
-  }
-  container.appendChild(col);
+
+function removeNodes() {
+  const nodes = document.querySelectorAll(".container div");
+  nodes.forEach((element) => {
+    element.remove();
+  });
 }
 
-const nodes = document.querySelectorAll(".node");
-nodes.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.classList.add("selected");
+function turnOnEtch() {
+  const nodes = document.querySelectorAll(".node");
+  nodes.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      element.classList.add("selected");
+    });
   });
+}
+
+function buildGrid(numOfRows) {
+  removeNodes();
+  for (let i = 0; i < numOfRows; i++) {
+    const col = document.createElement("div");
+    col.classList.add("column");
+    for (let j = 0; j < numOfRows; j++) {
+      const row = document.createElement("div");
+      row.classList.add("node");
+      col.appendChild(row);
+    }
+    container.appendChild(col);
+  }
+  turnOnEtch();
+}
+
+const gridSizeBtn = document.querySelector("#grid-size");
+gridSizeBtn.addEventListener("click", () => {
+  let inRange = false;
+  while (!inRange) {
+    let numOfRows = prompt("What size grid do you want? (1 - 100)");
+    if (numOfRows > 0 && numOfRows < 100) {
+      buildGrid(numOfRows);
+      inRange = true;
+    } else {
+      alert("Invalid input, please enter a number between 1 and 100");
+    }
+  }
 });
+
+//Build an initial 8 x 8 grid
+buildGrid(8);
